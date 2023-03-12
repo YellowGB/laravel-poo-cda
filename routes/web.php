0e442a5987d85809d5a29ba680f8762fc4dc6c9a
YellowGB <yellowchickencorp@gmail.com>
2023-03-12 23:16:46 +0100

    jetstream

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::get(
+        '/students',
+        [StudentController::class, 'index'],
+    )->name('students.index');
 });
